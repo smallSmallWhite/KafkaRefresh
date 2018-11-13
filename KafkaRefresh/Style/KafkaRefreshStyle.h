abcb9484b6113dced89a106f0b1c145ebefb1131
马鹏 <18291820539@163.com>
2018-11-13 10:57:25 +0800

    去掉测试代码

--- KafkaRefresh/Style/KafkaRefreshStyle.h
+++ KafkaRefresh/Style/KafkaRefreshStyle.h
@@ -25,7 +25,6 @@ typedef NS_ENUM(NSInteger,KafkaRefreshStyle) {
 	KafkaRefreshStyleAnimatableRing, 
 	KafkaRefreshStyleAnimatableArrow,
     KafkaRefreshStyleAnimatableProgressiveRing,
-    KafkaRefreshStyleAnimatbleNone,
 };
 
 
